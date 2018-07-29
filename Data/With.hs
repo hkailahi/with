c15a8e0079d0e72d3971dd5058123b051214b72d
Murat Kasimov <iokasimov.m@gmail.com>
2018-07-29 22:27:31 +0300

    Add Show instance to With type

--- Data/With.hs
+++ Data/With.hs
@@ -1,9 +1,16 @@
 module Data.With (type (:&:)((:@:), (:#:)), With) where
 
+import "base" Data.Semigroup (Semigroup((<>)))
+import "base" Text.Show (Show(show))
+
+type With = (:&:)
+
 infixl 1 :&:
 
 data (:&:) a b where
 	(:@:) :: a -> b -> a :&: b
 	(:#:) :: a :&: b -> c -> (a :&: b) :&: c
 
-type With = (:&:)
+instance (Show a, Show b) => Show (a :&: b) where
+	show (x :@: y) = show x <> " :&: " <> show y
+	show (x :#: y) = show x <> " :&: " <> show y
