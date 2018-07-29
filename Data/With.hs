d1c4104fcccd438a0808f858447598a01a15533c
Murat Kasimov <iokasimov.m@gmail.com>
2018-07-29 22:23:39 +0300

    Add open product type

--- Data/With.hs
+++ Data/With.hs
@@ -1,1 +1,9 @@
-module Data.With where
+module Data.With (type (:&:)((:@:), (:#:)), With) where
+
+infixl 1 :&:
+
+data (:&:) a b where
+	(:@:) :: a -> b -> a :&: b
+	(:#:) :: a :&: b -> c -> (a :&: b) :&: c
+
+type With = (:&:)
