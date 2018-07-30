ef6c6146dc00f680f38fe4a3dbfea87503d03f67
Murat Kasimov <iokasimov.m@gmail.com>
2018-07-30 16:08:35 +0300

    No dependencies!

--- Data/With.hs
+++ Data/With.hs
@@ -1,8 +1,5 @@
 module Data.With (type (:&:)((:@:), (:#:)), With) where
 
-import "base" Data.Semigroup (Semigroup((<>)))
-import "base" Text.Show (Show(show))
-
 type With = (:&:)
 
 infixl 1 :&:
@@ -10,7 +7,3 @@ infixl 1 :&:
 data (:&:) a b where
 	(:@:) :: a -> b -> a :&: b
 	(:#:) :: a :&: b -> c -> (a :&: b) :&: c
-
-instance (Show a, Show b) => Show (a :&: b) where
-	show (x :@: y) = show x <> " :&: " <> show y
-	show (x :#: y) = show x <> " :&: " <> show y
